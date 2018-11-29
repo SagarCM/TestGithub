c17ff799b2d9c3ea314797b22f6a5ab9b00f4d11
7000017571 <7000017571@INSISCILT-3609.ap.sony.com>
2018-11-29 13:55:58 +0530

    exact from eclipse

--- GithubTest/com/app/Demo.java
+++ GithubTest/com/app/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Main application");
-		System.out.println("Changing from eclipse");
+		System.out.println("Changing from eclipse1");
 
 	}
 
